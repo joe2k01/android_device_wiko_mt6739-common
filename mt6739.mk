172320192b02340a70fdb9fb8bc8ee87122dc16f
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-08-06 10:56:01 +0200

    mt6739-common: Inherit O MR1 common makefile
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- mt6739.mk
+++ mt6739.mk
@@ -6,6 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/wiko/mt6739-common/mt6739-common-vendor.mk)
