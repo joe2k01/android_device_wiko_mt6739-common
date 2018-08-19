6e264eb5ba74e4fa73cc1f819ebe499415ebf415
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-08-19 19:38:50 +0200

    mt6739-common: Set nvram_daemon domain permissive
     * nvram_daemon can't access fstab, which is unlabeled.
       The fstab is unlabeled, since when data is encrypted
       the daemon doesn't hit any denial this is suitable as
       a temporary solution.
    
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- mt6739.mk
+++ mt6739.mk
@@ -13,6 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/wiko/mt6739-common/mt6739-common-vendor.mk)
 
+# Common init scripts
+PRODUCT_PACKAGES += \
+    init.mt6739.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6739:/system/etc/fstab.mt6739
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
