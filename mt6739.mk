40d1a1b8a58190207a74d0f0a2aefee0bb569506
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-10-06 10:44:10 +0200

    mt6739-common: Initial commit
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ mt6739.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/wiko/mt6739-common/mt6739-common-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan
+
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
